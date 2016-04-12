5b732c7504a41ac5036f79e077b042f653fdbe20
Shandra Iannucci <shandrai@gmail.com>
2016-04-12 14:50:25 -0700

    Add warmer/colder functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 
 $(document).ready(function(){
 	
-	var number;
+	var number,
+		guess,
+		guessLimit,
+		guessDifference,
+		lastGuessDifference;
 
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -18,13 +22,9 @@ $(document).ready(function(){
 
   	$("form").submit(function(e) {
   		checkNumber($('input').val());
+  		$('input').val('');
   		e.preventDefault();
   	});
-
-  	function newGame() {
-  		number = generateNumber();
-  		console.log('New game started with secret number: ' + number);
-  	}
 
   	function generateNumber() {
   		return Math.floor((Math.random() * 100) + 1);
@@ -32,21 +32,34 @@ $(document).ready(function(){
 
   	function checkNumber(userNum) {
   		var guessLimit = 20;
-  		var guessDistance = Math.max(userNum, number) - Math.min(userNum, number);
+  		var guessDifference = Math.max(userNum, number) - Math.min(userNum, number);
 
-  		console.log(guessDistance);
-
-  		if (guessDistance == 0) {
+  		if (guessDifference == 0) {
   			$('#feedback').html('You got it!');
-  		}
-  		else if (guessDistance > guessLimit) {
-  			$('#feedback').html('Cold!');
-  		} else if (guessDistance < guessLimit) {
-  			$('#feedback').html('Hot!');
+  		} else if (!lastGuessDifference) {
+	  		if (guessDifference > guessLimit) {
+	  			$('#feedback').html('Cold!');
+	  		} else if (guessDifference < guessLimit) {
+	  			$('#feedback').html('Hot!');
+	  		}
+	  		lastGuessDifference = guessDifference;
+  		} else {
+  			if (guessDifference > lastGuessDifference) {
+  				$('#feedback').html('Colder :(');
+  			} else if (guessDifference < lastGuessDifference) {
+  				$('#feedback').html('Warmer :)');
+  			} else if (guessDifference == lastGuessDifference) {
+  				$('#feedback').html('Same guess.');
+  			}
+  			lastGuessDifference = guessDifference;
   		}
   	  	
-  	  	$('#count').html(parseInt($('#count').html()+1));
+  	  	$('#count').html(parseInt($('#count').html())+1);
   	  	$('#guessList').append('<li>' + userNum + '</li>');
+  	}
+
+  	function newGame() {
+  		number = generateNumber();
   	}
 
   	newGame();
